2877f484895610c276402797fdf9c3d6038e36eb
BaberMuyu <shenpengdyx@163.com>
2021-01-10 11:06:27 +0800

    Update remote_op.py

--- utils/remote_op.py
+++ utils/remote_op.py
@@ -22,20 +22,3 @@ def remote_scp(type, host_ip, remote_path, local_path, username, password):
     #     print('error')
     #     return False
 
-
-if __name__ == '__main__':
-    from multiprocessing import  Process
-    import time
-    path = '/home/zsp/projects/realtion_extract/joint_7830.txt'
-    remote_path = '/home/zsp/python/relation_extraction/saved_model/large/joint_7830.txt'
-    def my_copy():
-        remote_scp(type='remoteWrite',
-                   host_ip="192.168.1.213",
-                   remote_path=remote_path,
-                   local_path=path,
-                   username='zsp',
-                   password='shenpeng12.')
-
-    copy_p = Process(target=my_copy)
-    copy_p.start()
-    time.sleep(30)
